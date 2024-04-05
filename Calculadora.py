43a0528d03a710b21c6a66147a404c7c57e63fb8
alvaro944 <alvaro94cervantes@gmail.com>
2024-04-05 02:43:19 +0200

    Corrección del print del resultado al usar "="

--- Calculadora.py
+++ Calculadora.py
@@ -4,6 +4,7 @@ def pedirNumero():
     num1 = int(input(f"Introduce un número entero: "))
     try:
         if num1 > 0:
+            lista_numeros.append(num1)
             return num1
         else:
             raise ValueError (f"{num1} no es un número válido, introduce un número entero")
@@ -34,6 +35,15 @@ def division(num1):
         return division(num1)
 
     return num1 / num2
+    
+def resultadoPrint(resultado, lista_numeros, lista_operadores):
+    print(f"[+] El registro de las operaciones es ", end = " ")
+    
+    for numero, operador in zip(lista_numeros, lista_operadores):
+        print(numero, operador, end = " ")
+        
+    print(lista_numeros[-1], end = " ")  # Imprimir el último número
+    print(f"=", resultado)
 
 
 def validarOperador(resultado):
@@ -49,7 +59,10 @@ def validarOperador(resultado):
             return multiplicacion(resultado)
 
         elif operador == "/":
-            return resta(resultado)
+            return division(resultado)
+            
+        elif operador == "=":
+            return resultadoPrint(resultado, lista_numeros, lista_operadores)
 
         elif operador == "s":
             return "s"
@@ -59,10 +72,24 @@ def validarOperador(resultado):
     except ValueError as e:
         print(f"[!] Error: ", e)
         return validarOperador()
+        
+    finally:
+        if operador != "s" and operador != "=": # Añado los operadores a la lista si sigo realizando operaciones
+            lista_operadores.append(operador)
 
+        elif operador == "=": # Vacío las listas al usar "="
+            lista_numeros.clear()
+            lista_operadores.clear()
 
+        else:
+            print("traza finally")
+
+# Declaración de variables
+lista_numeros = []
+lista_operadores = []
 operador = ""
 resultado = None
+
 while operador != "s":
 
     if resultado is None:
@@ -72,5 +99,11 @@ while operador != "s":
     if operador != "s":
         resultado = operador
 
+resultadoPrint(resultado, lista_numeros, lista_operadores)
+print(lista_operadores)
+print(lista_numeros)
 print("Salimos del programa")
-print(f"[+] El resultado de las operaciones es {resultado}")
+print("Traza de la lista de números", lista_numeros)
+print("Traza de la lista de operadores", lista_operadores)
+
+# Añadir al bloque finally un condicional para clear las listas y arrglar lo que ya hay
